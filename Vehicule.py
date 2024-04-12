439b420ec342473f85eb65f067651be98c7e43a7
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-12 14:05:28 +0200

    first commit

---
+++ Vehicule.py
@@ -0,0 +1,13 @@
+"""
+A basic class using class attributes
+"""
+class Vehicule:
+    """
+     class don't have to be instanciate to have acces to attributes
+    """
+    mark_v="Toyota"
+    color_v="red"
+
+#Test
+print(Vehicule.mark_v)
+print(Vehicule.color_v)
