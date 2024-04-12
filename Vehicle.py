dc9ac967a8b206aeb8dac9e08b6689457dffa66d
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-12 15:03:36 +0200

    Distinguish class attributes and instanciates attributes

--- Vehicle.py
+++ Vehicle.py
@@ -16,3 +16,21 @@ vehicle_01=Vehicle()
 vehicle_02=Vehicle()
 print(vehicle_01.mark_v)
 print(vehicle_02.mark_v)
+
+#Modify  only class attribute and test, what changed ?
+Vehicle.mark_v="Suziki"
+print(Vehicle.mark_v)
+print(vehicle_01)
+print(vehicle_02)
+
+#Modify  only class instantiated and test, what changed ?
+vehicle_01.mark_v="Prado"
+
+print(Vehicle.mark_v)
+print(vehicle_01.mark_v)
+print(vehicle_02.mark_v)
+
+#in conclusion: instantiate attribute only affects itself(the object instanciated)
+# while class attribute affects the whole both [instanciate and class attributes]
+
+#N.B:most of the case, it is recommended to use instanciate attribute for the specifications reasons
