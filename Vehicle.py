aadcb5e37b374f610feaad28dcd623e4ddff1e8f
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-12 14:11:15 +0200

    class renamed

---
+++ Vehicle.py
@@ -0,0 +1,13 @@
+"""
+A basic class using class attributes
+"""
+class Vehicle:
+    """
+     class don't have to be instanciated to have acces to attributes
+    """
+    mark_v="Toyota"
+    color_v="red"
+
+#Test
+print(Vehicle.mark_v)
+print(Vehicle.color_v)
