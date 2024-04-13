da3d9bf8a5b293dcc3c2c70b1485d83b65ee98df
Bonevy BEBY <bonevybeby@gmail.com>
2024-04-13 08:51:01 +0200

    added:static methods:decorator staticmethod

--- Vehicle2.py
+++ Vehicle2.py
@@ -4,7 +4,9 @@ Very simple to manipulate and easy comparing to the instance methods(because you
 remember of attributes contrary to other one which is to remember of the attributes
 """
 class Vehicle2:
+    nb_vehicle_created=0
     def __init__(self, mark, speed,price):
+        Vehicle2.nb_vehicle_created += 1
         self.mark=mark
         self.speed=speed
         self.price=price
@@ -16,8 +18,15 @@ class Vehicle2:
     def suziki(cls):
         return cls(mark="Suziki",speed=250,price=200000)
 
+    #static method
+    @staticmethod
+    def show_number_vehicle():
+        print(f"You have {Vehicle2.nb_vehicle_created} in your garage")
+
 toyo= Vehicle2.toyota()
 suzi= Vehicle2.suziki()
 
 print(toyo.price)
 print(suzi.price)
+
+Vehicle2.show_number_vehicle()
